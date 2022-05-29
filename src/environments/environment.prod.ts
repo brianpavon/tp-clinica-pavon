7db40639b8ba4df20d40c02743d1f52edb3b172f
brianpavon <brian.pavon@hotmail.com>
2022-05-29 17:39:05 -0300

    agrego favicon,bootstrap

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'tp-clinica-pavon',
+    appId: '1:217517199631:web:de760b5d983d99af1cee7a',
+    storageBucket: 'tp-clinica-pavon.appspot.com',
+    apiKey: 'AIzaSyBzbcwINbfB-DNidcukmO5npzCx8c-QWe8',
+    authDomain: 'tp-clinica-pavon.firebaseapp.com',
+    messagingSenderId: '217517199631',
+  },
   production: true
 };
