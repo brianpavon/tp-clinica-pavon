6cd32f794f3631cba6b5b70da5046af3bc5b9473
brianpavon <brian.pavon@hotmail.com>
2022-06-07 23:49:37 -0300

    ok tabla pacientes

--- src/app/pages/dashboard/usuarios/usuarios.component.ts
+++ src/app/pages/dashboard/usuarios/usuarios.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuarios } from 'src/app/inerfaces/usuarios/usuarios';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-usuarios',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ['nombre', 'apellido', 'dni', 'email', 'edad','obraSocial','estado'];
+  todosLosPacientes : Usuarios[] = []
+  constructor(private userServ:UsuariosService) { }
 
   ngOnInit(): void {
+    this.cargarPacientes();
   }
+
+  cargarPacientes(){
+    this.userServ.traerUsuarios().subscribe(
+      usuarios=>{
+        this.todosLosPacientes = usuarios;
+        this.todosLosPacientes = this.todosLosPacientes.filter(paciente => paciente.rol === 'paciente');
+      }
+    )
+  }
+
+  cambiarEstados(event:any,id:any){
+    //console.log(event.source.name);
+    let atributo = event.source.name;
+    let valor = event.checked;
+    const data = {   
+      [atributo]: valor
+  
+    };
+    //console.log(data);
+    this.userServ.actualizarUsuario(data,id);
+  }
+ 
 
 }
